aa5964d9e25f9eadaad561c7e818a3c20e976c38
O1Gus <gustavorf106@gmail.com>
2025-05-20 21:56:49 -0300

    Foi criado medotos de busca da arvore

---
+++ src/No.java
@@ -0,0 +1,10 @@
+public class No {
+    Integer conteudo;
+    No filhoDireito, filhoEsquerdo;
+
+    public No(Integer conteudo) {
+        this.conteudo = conteudo;
+        this.filhoEsquerdo = null;
+        this.filhoDireito = null;
+    }
+}
--- src/arvore.java
+++ src/arvore.java
@@ -1,11 +1,31 @@
 public class arvore {
-    Integer conteudo;
-    no filhoDireito;
-    no filhoEsquerdo;
+    No raiz;
 
-    public arvore(Integer conteudo, no filhoDireito, no filhoEsquerdo) {
-        this.conteudo = conteudo;
-        this.filhoDireito = filhoDireito;
-        this.filhoEsquerdo = filhoEsquerdo;
+    public arvore (){
+        this.raiz = null;
+    }
+
+    public int contarNos(No node){
+       if(node == null){
+           return 0;
+       }else{
+           return 1 + contarNos(node.filhoEsquerdo) + contarNos(node.filhoDireito);
+       }
+    }
+
+    public void buscarPreOrderm(No node){
+        if (node != null) {
+            System.out.println(node.conteudo + " ");
+            buscarPreOrderm(node.filhoEsquerdo);
+            buscarPreOrderm(node.filhoDireito);
+        }
+    }
+
+    public void buscarEmOrdem(No node){
+        if (node != null){
+            buscarEmOrdem(node.filhoEsquerdo);
+            System.out.println(node.conteudo + " ");
+            buscarEmOrdem(node.filhoDireito);
+        }
     }
 }
